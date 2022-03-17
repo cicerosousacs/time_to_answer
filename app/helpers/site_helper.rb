f9d93f927fca9d975afdce7d3c9b1dec9e895cab
cicerosousacs <93005817+cicerosousacs@users.noreply.github.com>
2022-03-16 22:24:09 -0300

    ok
    ok

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,14 @@
 module SiteHelper
+
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Ùltimas perguntas cadastradas..."
+    when 'questions'
+      "Resultado encontrado para \"#{params[:term]}\"..."
+    when 'subject'
+      "Mostrando questôes para o assunto \"#{params[:subject]}\"..."
+    end
+  end
+
 end
